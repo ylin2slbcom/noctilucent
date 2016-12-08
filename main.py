b89ac5a6f43c0f137c86333f308dfff7459a98c6
Deepyaman Datta <DDatta2@slb.com>
2016-12-08 11:52:22 -0600

    str change so dumb gahhhhh

--- main.py
+++ main.py
@@ -95,7 +95,7 @@ class Publish(Resource):
     @api.expect(topic_model, validate=True)
     def post(self, id):
         try:
-            capital = countries.Capitals().get_by_id(str(id))
+            capital = countries.Capitals().get_by_id(id)
             if capital is None:
                 return {"code": 0,  "message": "capital not found"}, 404
             pubsub_client = pubsub.Client()
